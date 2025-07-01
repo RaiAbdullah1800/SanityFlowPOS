152993c8f4ab2ad8a6a337c8ea1a0a8c36cef3b1
RaiAbdullah <nawazabdullah1800@gmail.com>
2025-07-01 16:56:48 +0500

    added the auth routes into the main file for app.include_router

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@
 from fastapi import FastAPI
 from app.db.db import engine
 from app.db import models
-from app.api.routes import auth
+from app.api.routes import auth, admin
 
 
 # Create tables
 models.Base.metadata.create_all(bind=engine)
-app = FastAPI()
+app = FastAPI(title="SanityFlow POS", version="1.0.0")
+
+# Include routers
 app.include_router(auth.router)
+app.include_router(admin.router)
 
 @app.get("/")
 def root():
