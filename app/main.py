7445aa91c20fb5845a2cea1858c86b8bbfb0e519
RaiAbdullah <nawazabdullah1800@gmail.com>
2025-07-03 15:10:38 +0500

    added cors middleware to allow all the origins for all type of request

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
-# app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.db import engine
 from app.db import models
 from app.api.routes import auth, admin
 
-
 # Create tables
 models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="SanityFlow POS", version="1.0.0")
+
+# 🚨 Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 👈 Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers
 app.include_router(auth.router)
