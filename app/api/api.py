9bd98269a6b40f86adbc19bd1e1049b4b86dc1c5
RaiAbdullah <nawazabdullah1800@gmail.com>
2025-08-18 10:17:52 +0500

    updated the api file to add the shopper routes in the app api route

--- app/api/api.py
+++ app/api/api.py
@@ -6,8 +6,11 @@ from app.api.routes import (
     cashier,
     cashier_dashboard,
     shared,
-    admin_dashboard
+    admin_dashboard,
+    shopper
 )
+
+api_router = APIRouter()
 
 def include_routers(app):
     """
@@ -20,12 +23,15 @@ def include_routers(app):
         FastAPI: The configured FastAPI application
     """
     # Include all routers
-    app.include_router(auth.router)
-    app.include_router(admin.router)
-    app.include_router(categories.router)
-    app.include_router(cashier.router)
-    app.include_router(cashier_dashboard.router)
-    app.include_router(shared.router)
-    app.include_router(admin_dashboard.router)
+    api_router.include_router(auth.router)
+    api_router.include_router(admin.router)
+    api_router.include_router(cashier.router)
+    api_router.include_router(categories.router)
+    api_router.include_router(admin_dashboard.router)
+    api_router.include_router(cashier_dashboard.router)
+    api_router.include_router(shared.router)
+    
+    api_router.include_router(shopper.router, prefix="/shoppers", tags=["Shoppers"])
+    app.include_router(api_router)
     
     return app
